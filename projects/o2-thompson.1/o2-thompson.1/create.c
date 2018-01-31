82d2b11769e8bfe783a9be005a39c3deb57c2715
Chris Thompson <cjt7g6@umsl.edu>
2018-01-31 00:35:17 -0600

    Project-1: added create.c, create.h, function to print usage statement. main() now checks for correct number of arguments

---
+++ projects/o2-thompson.1/o2-thompson.1/create.c
@@ -0,0 +1,9 @@
+//
+//  create.c
+//  o2-thompson.1
+//
+//  Created by Magnificent Bastard on 1/30/18.
+//  Copyright © 2018 Chris Thompson. All rights reserved.
+//
+
+#include "create.h"
---
+++ projects/o2-thompson.1/o2-thompson.1/create.h
@@ -0,0 +1,13 @@
+//
+//  create.h
+//  o2-thompson.1
+//
+//  Created by Magnificent Bastard on 1/30/18.
+//  Copyright © 2018 Chris Thompson. All rights reserved.
+//
+
+#ifndef create_h
+#define create_h
+
+
+#endif /* create_h */
