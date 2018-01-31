42fed9e806c51b9cc35bb12b3eb15c2ed3dc3d56
Chris Thompson <cthompson@armsbusinesssolutions.com>
2018-01-30 19:55:13 -0600

    Initial commit of project 1. Set up the directory and project structure. CT

---
+++ projects/o2-thompson.1/o2-thompson.1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  o2-thompson.1
+//
+//  Created by Chris Thompson on 1/30/18.
+//  Copyright © 2018 Chris Thompson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
